409d96b4d3bf1e665c880e92ca95d6e66ff88e66
Ingley <michael.ingley@gmail.com>
2021-04-19 18:56:34 -0700

    adds insert, db init, save methods, todo item completion still WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::collections::HashMap;
-
+use std::{collections::HashMap, io::Read};
+use std::str::FromStr;
 
 
 fn main() {
@@ -7,6 +7,16 @@ fn main() {
     let item = std::env::args().nth(2).expect("Please specify an item to act on");
 
     println!("{:?}, {:?}", action, item);
+
+    let mut todo = Todo::new().expect("db init failed");
+
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("saved succesfully"),
+            Err(why) => println!("ERR @ {}", why),
+        }
+    }    
 }
 
 struct Todo {
@@ -14,9 +24,46 @@ struct Todo {
 }
 
 impl Todo {
+
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k,v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo { map })
+    }
+
     fn insert(&mut self, key: String) {
         //insert new item into map
         self.map.insert(key, true);
+    }
+
+    //owns Todo struct implmentation, this prevents posthumous inserts after save
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            // for every key/value pair stored in map at time of save call 
+            // write a new line in txt file to save application state
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content)
+    }
+
+    fn complete(&mut self, key: &String) -> Option<()> {
+        match self.map.get_mut(key) {
+            Some(v) => Some(*v = false),
+            None => None,
+        }
     }
 }
 
